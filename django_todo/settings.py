4004ae415a5a13d9886d3cba0efea9d07f5fdf28
Culan O'Meara <culanomeara@gmail.com>
2023-02-17 13:26:43 +0000

    Add env vars and auto updates

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hm5snzjeboo@53!9q%g$blxd$s!em9&*ng5!qpve71=m3iet^6'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hm5snzjeboo@53!9q%g$blxd$s!em9&*ng5!qpve71=m3iet^6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['culanski-django-todo-app.herokuapp.com']
-
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
